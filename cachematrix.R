cb40acb1aafde429de15e60937a058735beb8ae3
sergiotallon <sergio.tallon@gmail.com>
2015-08-23 11:10:26 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,22 @@
 
 ## The function below should create a special matrix to cache its inverse.
 
-makeCacheMatrix <- function(X = matrix()) {
-inverse <- NULL
-set <- function(Y){
-	X <<- Y
-	inverse <<- NULL
-	}
-get <- function() X
-setinverse <- function(Inverse) inverse <<- Inverse
-getinverse <- function() inverse
-list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <-- NULL
+        }
+        #function to set matrix
+        get <- function() x
+        #function to get matrix
+        setinverse <- function(inverse) m <<- inverse
+        #function to set inverse of x
+        getinverse <- function() m
+        #function to get inverse of x
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -20,27 +26,17 @@ list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 ##If the inverse has already been calculated (and the matrix has not changed), 
 ##then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(X, ...) 
-{
-if(require("corpcor")){
-	print("corpcor is loaded correctly")
-	} else {
-		print("trying to install corpcor")
-		install.packages("corpcor")
-		if(require(corpcor)){
-			print("corpcor installed and loaded")
-			} else {
-			stop("could not install corpcor")
-			}
-		}
-inverse <- X$getinverse()
-if(!is.null(inverse)){
-	message("matrix is in memory")
-	return(inverse)
-	}
-message("inverse is not in memory so the inverse (if exist) is gonna be computed")
-data <- X$get()
-inverse <- pseudoinverse(data, ...)
-X$setinverse(inverse)
-inverse
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        #if function checks if inverse of matrix exists, if it does, it returns cached inverse
+        
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+        ## Return a matrix that is the inverse of 'x'
 }
